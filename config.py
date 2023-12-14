cb3d11d8f704253e29849a0b7bfb2b059b92bd1d
Dylan Krim <dkrim@lion.lmu.edu>
2023-12-13 19:47:32 -0800

    Dylan - Added config.py and updated syllacal.py

---
+++ config.py
@@ -0,0 +1,4 @@
+openai_api_key = "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"
+syllabus_directory = "syllabi"
+user = 'syllacalcmsi3801@gmail.com'
+app_password = 'mfdi dbwb dvbo ztru'
--- syllacal.py
+++ syllacal.py
@@ -1,20 +1,21 @@
 import mailer
 import openai
 import conversion
+import os
+import config
 
+openai.api_key = config.openai_api_key
+syllabus_directory = config.syllabus_directory
+user = config.user
+app_password = config.app_password
+recipient = input("Enter your email address: ")
 
-# TODO: put all these hardcoded variables/api-keys in a "config" file and import/read the config file^.
-openai.api_key = "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"
-syllabus_directory = "syllabi"
-user = 'syllacalcmsi3801@gmail.com'
-app_password = 'mfdi dbwb dvbo ztru'
-recipient = 'dylankrim@gmail.com'  # this is hardcoded directly in code if want to change email recipient.
+try:
+    ics_files = conversion.syllabus_conversion(syllabus_directory)
+    mailer.ics_mailer(ics_files, user, app_password, recipient)
 
+    for file in ics_files:
+        os.remove(file)
 
-ics_files = conversion.syllabus_conversion(syllabus_directory)
-# TODO: error handling for if this^ fails.
-mailer.ics_mailer(ics_files, user, app_password, recipient)
-
-# TODO: delete ics_files from directory, since mailer has sent them and are no longer needed.
-# ^(When you run the program and create the ics files, a copy of the ics files appear in your
-# directoty, which you will see if you run the program. Delete these AFTER they are sent out.)
+except Exception as e:
+    print(f"Error occurred: {e}")
